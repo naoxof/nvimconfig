ead8abadf995714d90c808c9d30013f57b332c06
Yoshinori Kasaya <yoshinori2314@gmail.com>
2026-02-22 10:29:48 +0900

    パーサー追加

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,14 +1,18 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter").setup({
-            ensure_installed = { "markdown", "markdown_inline" },
+        require("nvim-treesitter").install({
+            "markdown", "markdown_inline",
+            "swift", "typescript", "javascript",
+            "html", "css", "python", "lua",
         })
         vim.api.nvim_create_autocmd("FileType", {
-            pattern = "markdown",
+            group = vim.api.nvim_create_augroup("treesitter-start", {}),
             callback = function()
-                vim.treesitter.start()
+                pcall(vim.treesitter.start)
             end,
         })
     end,
